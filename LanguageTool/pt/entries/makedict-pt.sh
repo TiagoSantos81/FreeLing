58d1cc4ade2e7f3379b84aab17cc3e8855b572e3
TiagoSantos <tiagofsantos81@sapo.pt>
2017-08-09 19:02:41 +0100

    [pt] dictionary builder script improvements

--- LanguageTool/pt/entries/makedict-pt.sh
+++ LanguageTool/pt/entries/makedict-pt.sh
@@ -5,10 +5,11 @@ cat nouns >> temp
 cat verbs >> temp
 cat propernouns_geo >> temp
 cat propernouns_people >> temp
+cat added >> temp
 sort temp > temp2
 # Remove comments
 sed -i -r 's/#.+//g' temp2
 # Remove empty lines
 sed -i '/^$/d' temp2
-cat temp2 | ./freeling2lt.pl > portuguese.dict.txt
+cat temp2 | sort | uniq | ./freeling2lt.pl > portuguese.dict.txt
 rm temp temp2
